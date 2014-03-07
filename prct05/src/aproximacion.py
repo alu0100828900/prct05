ae3dd05c76a51042ac8d1dad45a82e47eb0911e6
serezade gonzalez <alu0100828900@ull.edu.es>
2014-03-07 10:26:07 +0000

    Aproximacion de pi terminado

--- prct05/src/aproximacion.py
+++ prct05/src/aproximacion.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
+PI = 3.14159265358979323846264338327950288
 n = int(raw_input('Valor de n: '))
 sumatorio=0
 for i in range (1,n+1):
     a= float(i-1)/n
     b= float(i)/n
-    print 'subintervalo (%.5f,%.5f)'%(a,b)  
     x_i=(i-0.5)/float(n)  
-    print 'x_i: %f' %x_i  
     fx_i=4/(1+x_i**2)  
-    print 'fx_i: %f' %fx_i  
+    print 'subintervalo (%.5f,%.5f) x_i: %.5f fx_i: %.5f'%(a,b,x_i,fx_i)  
     sumatorio+=fx_i
 pi=sumatorio/n
-print 'El valor pi es %f' %pi
-print 'El valor de pi con 35 decimales es %.35f' %pi
+print 'El valor pi es %.35f' %pi
+print 'El valor de pi con 35 decimales es %.35f' %PI
